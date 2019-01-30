23e86d28fb4776d5c8d350dc6514048ce0c28b88
said shah <saidmysr20@aol.com>
2019-01-30 09:15:03 -0500

    created files

---
+++ index.js
@@ -0,0 +1,9 @@
+let arr = [2,5,4,99,6,7,43,9,23,53,64,76,34,75]
+let stringArr = ["b","f","z","l","u","a","e","w","q","c","j","d"]
+
+bubbleSort=(givenArr)=>{
+  console.log("its in here")
+}
+
+bubbleSort(arr)
+bubbleSort(stringArr)
