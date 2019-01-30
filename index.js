41166ad53ad065d1215a910c47c854d8a56d6e4a
said shah <saidmysr20@aol.com>
2019-01-30 09:21:51 -0500

    finished

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 let arr = [2,5,4,99,6,7,43,9,23,53,64,76,34,75]
 let stringArr = ["b","f","z","l","u","a","e","w","q","c","j","d"]
+console.log("before sort", arr)
+
+console.log("before sort", stringArr)
 
 bubbleSort=(givenArr)=>{
-  console.log("its in here")
+  let swapped;
+  do{
+    swapped = false;
+    for(let i = 0;i<givenArr.length;i++){
+      if(givenArr[i]>givenArr[i+1]){
+        let temp = givenArr[i]
+        givenArr[i] = givenArr[i+1]
+        givenArr[i+1] = temp
+        swapped=true;
+      }
+    }
+  }while(swapped)
+  return givenArr
 }
 
-bubbleSort(arr)
-bubbleSort(stringArr)
+console.log("after sort", bubbleSort(arr))
+
+
+console.log("after sort", bubbleSort(stringArr))
